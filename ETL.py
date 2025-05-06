33b253de988d84ca719961447cd58faa241b9a56
SecreShall <164751004+SecreShall@users.noreply.github.com>
2025-05-06 21:05:05 +0800

    Update

--- ETL.py
+++ ETL.py
@@ -4,7 +4,7 @@ import pandas as pd
 def exctract():
     # Extract all location data
     print(f"Extracting Location Data")
-    df_location = pd.DataFrame()
+    df_location = pd.DataFrame(columns=['id', 'name', 'dimension', 'residents'])
     for index in range(1,127):
         url = f"https://rickandmortyapi.com/api/location/?page={index}"
         raw_data = requests.get(url)
@@ -33,7 +33,7 @@ def exctract():
 
     # Extract all character data
     print(f"Extracting Character Data")
-    df_character = pd.DataFrame()
+    df_character = pd.DataFrame(columns=['id', 'name', 'status', 'species', 'type', 'gender', 'origin', 'location', 'episode_count'])
 
     for index in range(1, 827):
 
@@ -54,7 +54,7 @@ def exctract():
                         'gender': character['gender'],
                         'origin': character['origin']['url'],
                         'location': character['location']['url'],
-                        'episode': len(character['episode']),
+                        'episode_count': len(character['episode']),
                         }])
                     ], 
                     ignore_index=True
@@ -69,10 +69,9 @@ def transform():
     # Read location data
     df_location = pd.read_csv('Data/extracted_location_data.csv')
 
-    # Fill null values of columns and drop an unwated column
+    # Fill null values of columns 
     df_location['type'] = df_location['type'].fillna('unknown')
     df_location['dimension'] = df_location['dimension'].fillna('unknown')
-    df_location = df_location.drop(columns=['Unnamed: 0'])
 
     # Character ---------------------------------------------------
     # Read character data
@@ -84,13 +83,15 @@ def transform():
     df_character['location'] = df_character['location'].str.split('/').str[5].fillna(0).astype(int)
 
     # remove duplicates of character data
-    clean_character = df_character.drop_duplicates(subset=['name','status','species','type','gender', 'origin', 'location','episode'], keep='first')
+    clean_character = df_character.drop_duplicates(subset=['name','status','species','type','gender', 'origin', 'location','episode_count'], keep='first')
 
-    # Reset the id to remove unique id gap and drop an unwated column
+    # Reset the id to remove unique id gap
     clean_character = clean_character.reset_index(drop=True) 
     clean_character['id'] = range(1, len(clean_character) + 1)
-    clean_character = clean_character.drop(columns=['Unnamed: 0'])
 
     # Output cleaned data as csv
     df_location.to_csv('Data/transformed_location_data.csv')
     clean_character.to_csv('Data/transformed_character_data.csv')
+
+exctract()
+transform()
